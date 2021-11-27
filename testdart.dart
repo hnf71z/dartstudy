6d0bed37cff580be014a50ae33603e9b0bedf4da
Hanif Abdusy Syakur <hanifabdusy914s@gmail.com>
2021-11-27 09:14:07 +0700

    first commit

---
+++ testdart.dart
@@ -0,0 +1,14 @@
+void main() {
+  var sapa = "Hello World !";
+  String name = "Hanif Abdusy Syakur";
+  int age = 16;
+  double height = 155;
+  bool Ispelajar = true;
+  var alamat = "Jl Limas III No.11";
+
+  print(sapa);
+  print('Halo $name');
+  print('Umur anda : $age');
+  print('Tinggi badan anda : $height');
+  print('Alamat : $alamat');
+}
